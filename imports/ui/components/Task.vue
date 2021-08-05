f856ba3d7edd2f6fe7fed8020166eec5e1d6912b
Vinsensius Angelo <vafrcor2007@yahoo.co.id>
2021-08-05 09:30:09 +0700

    step-4

--- imports/ui/components/Task.vue
+++ imports/ui/components/Task.vue
@@ -1,12 +1,44 @@
 <template>
-	<li>{{ this.task.text }}</li>
+	<li v-bind:class="taskClassName">
+    <input 
+      type="checkbox" 
+      readonly
+      v-bind:checked="!!this.task.checked" 
+      @click="toggleChecked"
+    />
+    <span class="text">{{ this.task.text }}</span>
+    <button class="delete" @click="deleteThisTask">x</button>
+  </li>
 </template>
 
 <script>
+  import { TasksCollection } from '../../api/collections/Tasks';
 	export default {
 		props: ["task"],
 		data(){
 			return {};
-		}
+		},
+    computed: {
+      taskClassName: function(){
+        return this.task.checked? "checked" : "";
+      }
+    },
+    methods:{
+      toggleChecked: function(){
+        // Set the checked property to the opposite of its current value
+        TasksCollection.update(this.task._id, {
+          $set: { checked: !this.task.checked }
+        });
+      },
+      deleteThisTask: function(){
+        TasksCollection.remove(this.task._id);
+      }
+    }
 	}
 </script>
+
+<style>
+  button.delete{
+    color: red;
+  }
+</style>
